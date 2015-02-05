2f94314408275b15b464162c210e0b7e611b5de1
Luke Clewlow <luke.clewlow@gmail.com>
2015-02-05 08:56:40 +0000

    added JSON love

--- server.js
+++ server.js
@@ -4,8 +4,15 @@ var http = require('http');
 var server = http.createServer(app);
 var port = 9999;
 
+app.set('view engine', 'ejs');
+app.set(express.static(__dirname + '/public'));
+
 app.get('/', function(request, response){
-	response.send("hello world");
+	response.render('index', request.query);
+});
+
+app.get('/reflect.json', function(request, response){
+	response.render('reflect', {query: request.query});
 });
 
 server.listen(port, function(){
--- test/homepage_test.js
+++ test/homepage_test.js
@@ -1,10 +1,26 @@
 describe("homepage", function(){
+
+	var host = 'http://localhost:9999';
+
 	before(function(){
-		casper.start('http://localhost:9999');
+		casper.start(host);
 	});
-	it('should say hello world', function(){
-		casper.then(function(){
-			expect('body').to.have.text("hello world");
+
+	// it('should say hello world', function(){
+	// 	casper.then(function(){
+	// 		expect('body').to.have.text("hello world");
+	// 	});
+	// });
+
+	it("should greet people individually", function(){
+		casper.thenOpen(host + '?name=luke', function(){
+			expect('body').to.have.text('hello luke');
 		});
 	});
+
+	it("shuld give me some JSON love", function(){
+		casper.thenOpen(host + '/reflect.json?name=luke', function(){
+			expect('body').to.have.text('{"name":"luke"}')
+		});
+	});	
 });
